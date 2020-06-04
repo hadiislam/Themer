8cba69e69148827fe4fb0f7cae045599aa858110
Hadi Islam <36949346+hadiislam@users.noreply.github.com>
2020-06-05 04:08:01 +0600

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,1 +1,30 @@
 #Remove
+
+#Made By Hadi Islam
+#Made in Bangladesh With ❤
+
+#colors
+R='\033[1;31m'
+B='\033[1;34m'
+C='\033[0;36m'
+G='\033[1;32m'
+W='\033[1;37m'
+Y='\033[1;33m'
+
+#logo 
+cd
+rm -rf /data/data/com.termux/files/usr/etc/bash.bashrc
+cd
+cd Themer
+cd core
+cd rem
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+cd
+
+
+#color
+
+cd
+rm -rf .termux
+cd
+clear
